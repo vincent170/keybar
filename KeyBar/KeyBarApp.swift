a03d67f9f50399e69466c007e610c293c2fb4666
Vincent <VincentHao100@hotmail.com>
2026-08-12 11:07:27 +0100

    Initial Commit

---
+++ KeyBar/KeyBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyBarApp.swift
+//  KeyBar
+//
+//  Created by Vincent Hao on 12/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct KeyBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
